042f9c31d7894ed672343f3436b2b8f04e094ba5
funnypuni <vunnyvivo@gmail.com>
2026-06-04 12:45:30 +0700

    pisahkan footer

---
+++ footer.php
@@ -0,0 +1,28 @@
+<footer class="footer">
+        <h2>Contact Us</h2>
+        <div class="contact-box">
+            <div class="contact-item">
+                <div class="info">
+                    <img src="img/Whatsapp.png" alt="logo" width="30px" height="30px">
+                    <span>+62 895 3285 37054</span>
+                </div>
+
+                <div class="info">
+                    <img src="img/Instagram1.png" alt="logo" width="20px" height="20px">
+                    <span>@loop.and.soul</span>
+                </div>
+            </div>
+
+            <div class="contact-item">
+                <div class="info">
+                    <img src="img/Whatsapp.png" alt="logo" width="30px" height="30px">
+                    <span>+62 857 5006 4389</span>
+                </div>
+
+                <div class="info">
+                    <img src="img/Instagram1.png" alt="logo" width="20px" height="20px">
+                    <span>@looplooms.id</span>
+                </div>
+            </div>
+        </div>
+</footer>
