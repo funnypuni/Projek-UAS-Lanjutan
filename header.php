82fb59ee3f782a163faf683f980610fe9647caff
funnypuni <vunnyvivo@gmail.com>
2026-06-08 15:41:39 +0700

    edit  accesoris

---
+++ header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="id">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Projek UTS</title>
+    </head>
+    <body>
+        <header class="navbar">
+            <nav>
+                <div class="logo">
+                    <img src="img/logo.png" width="150px" height="60px">
+                </div>
+
+                <div class="menu">
+                    <button class="btn" onclick="window.location.href='home.html'">Home</button>
+                    <button class="btn" onclick="window.location.href='shop.html'">Shop</button>
+                    <button class="btn" onclick="window.location.href='custom.html'">Custom</button>
+                </div>
+            </nav>
+        </header>
