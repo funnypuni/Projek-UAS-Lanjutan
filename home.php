dfe014c3685edf12c961bc2a943b4d3f7ab0a1e8
alfin25-hub <alfin25@itbss.ac.id>
2026-06-17 09:30:00 +0700

    update home

--- home.php
+++ home.php
@@ -14,17 +14,21 @@ include 'header.php';
             <div class="foto">
                 <div class="card">
                     <h3 class="judul">Owner Loop & Soul</h3>
+                    <br>
                     <img src="images/owner_photo/funny_rl.jpeg" width="450" height="333">
                     <p>Hai, aku Funny, owner dari Loop & Soul! Awalnya, aku ikut workshop crochet dan meronce gelang karna mau penuhin S-Core dan udah lama juga sih aku pengen belajar crochet. Lama-lama aku sama Sella jadi sering beli beads dan benang gitu deh. Akhirnya, aku bikin Loop & Soul buat jadi tempat aku tuangin semua ide-ide aku deh! Xixixi.</p>
                 </div>
 
                 <div class="card">
                     <h3 class="judul">Owner Loop & Soul</h3>
+                    <br>
                     <img src="images/owner_photo/sella_rl.jpeg" width="450" height="333">
                     <p>Hai, aku Sella, owner dari Loop & Loom’s! Kalau aku awalnya karena liat rajutan yang lucu-lucu banget, jadi pengen coba sendiri. Aku ikut workshop terus lanjut belajar sendiri di rumah dari yang cuma coba-coba, malah jadi suka banget dan keterusan bikin ini itu sampai akhirnya berani bikin brand sendiri, sekarang rajut tuh bukan cuma hobi lagi, tapi udah jadi hal yang aku jalanin dengan seneng banget!</p>
                 </div>
             </div>
-
+            <br>
+            <h2 align="center">Poster</h2>
+            <br>
             <div class="poster">
             <img src="images/poster/poster.jpeg" width="333" height="500">
             </div>
