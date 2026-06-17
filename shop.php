8d80f0e3e31f1cc4210d38ad2fa6067be21fce79
alfin25-hub <alfin25@itbss.ac.id>
2026-06-17 08:52:57 +0700

    update

--- shop.php
+++ shop.php
@@ -18,10 +18,7 @@ while($row = mysqli_fetch_assoc($query)){
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Shop - Loop & Soul</title>
-
-
-
-
+<link rel="stylesheet" href="css/shop.css">
 </head>
 <body>
 
